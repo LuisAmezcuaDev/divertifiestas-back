6010f821800d0947c9509fdc82a398bc58cf4352
lu1s4nt0n10 <luisamezcuavargas@gmail.com>
2026-01-15 03:42:34 -0600

    Fix  add railway to CSRF trusted origin

--- divertifiestasBack/settings.py
+++ divertifiestasBack/settings.py
@@ -141,3 +141,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://divertifiestas-back-production.up.railway.app', 'https://*.up.railway.app']
